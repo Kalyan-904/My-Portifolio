b2aa1f9499552be76adfa5c0ed676a7c90278d59
kalyan <gkalyanram904@gmail.com>
2023-02-01 14:52:59 +0530

    part-1 completed

---
+++ script.js
@@ -0,0 +1,7 @@
+/* ============= typing animation =============== */
+var typed = new Typed(".typing",{
+    strings:["","SDET","Software Engineer","Software Tester"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
